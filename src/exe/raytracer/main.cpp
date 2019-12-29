6a042fd04884bfe527caa431d530c66fbadf63c5
Phil Johnson <phytoporg@gmail.com>
2019-12-28 16:44:03 -0800

    Raytracer loads config params from json file. Fixes #3

--- src/exe/raytracer/main.cpp
+++ src/exe/raytracer/main.cpp
@@ -2,8 +2,11 @@
 #include <qtwin/RenderWidget.h>
 #include <qtwin/CameraIntrinsics.h>
 
+#include <fstream>
 #include <memory>
 #include <vector>
+
+#include <json/json.hpp>
 
 #include <glm/glm.hpp>
 
@@ -18,24 +21,58 @@ static float DegreesToRadians(float degrees)
 
 int main(int argc, char** argv)
 {
+    if (argc < 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " <path_to_config>" << std::endl;
+        return -1;
+    }
+
+    const std::string ConfigPath(argv[1]);
+    std::ifstream in(ConfigPath);
+
+    using json = nlohmann::json;
+    json config;
+
+    try 
+    {
+        in >> config;
+    }
+    catch (const json::parse_error& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return -1;
+    }
+
     QApplication app(argc, argv);
 
     //
     // Hardcoding camera parameters for now
     //
 
-    const glm::vec2 PrincipalPoint(0.0, 0.0);
+    const glm::vec2 PrincipalPoint(
+            config["px"].get<float>(), 
+            config["py"].get<float>());
 
-    const float FovHorizontal = DegreesToRadians(62.2f);
-    const float FovVertical   = DegreesToRadians(48.8f);
+    const float FovHorizontal =
+        DegreesToRadians(config["fov_horizontal"].get<float>());
+    const float FovVertical = 
+        DegreesToRadians(config["fov_vertical"].get<float>());
 
-    const float FocalLength{0.00304f}; // In meters
+    const float FocalLength{config["focal_length"].get<float>()};
 
     const float ax{2.0f * FocalLength * tanf(FovHorizontal / 2.0f)};
     const float ay{2.0f * FocalLength * tanf(FovVertical   / 2.0f)};
 
-    glm::vec3 RadialDistortion(5.01f, 2.01f, 4.01f);
-    glm::vec2 TangentialDistortion(0.02f, 0.01f);
+    //
+    // Just a little barrel distortion
+    // 
+    glm::vec3 RadialDistortion(
+            config["k0"].get<float>(),
+            config["k1"].get<float>(),
+            config["k2"].get<float>());
+    glm::vec2 TangentialDistortion(
+            config["t0"].get<float>(),
+            config["t1"].get<float>());
 
     const glm::vec2 SpatialResolution(ax, ay);
     const qtwin::CameraIntrinsics Intrinsics(
@@ -50,8 +87,14 @@ int main(int argc, char** argv)
         new RenderWidget(Intrinsics)
         );
 
-    spGL->SetChessboardRotation(glm::radians(0.0f), glm::radians(15.0f), 0.0f);
-    spGL->SetChessboardTranslation(0.0, 0.0, 1.0);
+    spGL->SetChessboardRotation(
+            glm::radians(config["yaw"  ].get<float>()),
+            glm::radians(config["pitch"].get<float>()),
+            glm::radians(config["roll" ].get<float>()));
+    spGL->SetChessboardTranslation(
+            config["tx"].get<float>(),
+            config["ty"].get<float>(),
+            config["tz"].get<float>());
     
     MainWindow window(spGL.get());
     spGL->setParent(&window);
